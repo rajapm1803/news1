a27a6b5bf44d09ed494ea283615a0ef0445c78a9
rajapm1803 <rajadev1897@gmail.com>
2023-09-12 10:07:18 +0530

    api change

--- src/Component/Dashboard.js
+++ src/Component/Dashboard.js
@@ -8,11 +8,12 @@ import { Cardview } from './Cardview'
 export const Dashboard = () => {
 
     const [news, setNews] = useState([])
+    const apiUrl = "https://newsapi.org/v2/everything?q=tesla&from=2023-08-12&sortBy=publishedAt&apiKey=b3389e4b2090499aa974e6d9e1098db3"
 
     useEffect(() => {
       const fetchData = async () => {
         try {
-          const response = await axios.get("https://newsapi.org/v2/everything?q=tesla&from=2023-08-11&sortBy=publishedAt&apiKey=b3389e4b2090499aa974e6d9e1098db3");
+          const response = await axios.get(apiUrl);
           setNews(response.data?.articles);
         } catch (error) {
           // Handle the error here, e.g., log it or set an error state.
@@ -21,7 +22,11 @@ export const Dashboard = () => {
       };
     
       fetchData();
-    }, []);
+      const interval = setInterval(() => {
+        fetchData();
+      }, 30 * 60 * 1000); 
+      return () => clearInterval(interval);
+    }, [apiUrl]);
 
   return (
     <Container fluid className="ps-5 pe-5">
